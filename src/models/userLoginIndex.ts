38d3fc3f87d44d9dc787408566b04c2dbf6e890d
shaowei.xu <shaowei.xu@shopee.com>
2023-04-24 00:25:51 +0800

    feat: add comments

--- src/models/userLoginIndex.ts
+++ src/models/userLoginIndex.ts
@@ -1,26 +1,48 @@
+// Import necessary modules
 import { signIn, signOut } from '@/services/userLoginIndex';
 import { useCallback, useState } from 'react';
 
+// Define a custom hook for user login functionality
 export default function useUserLoginIndex() {
+  // Define state for login loading status
+  // 定义用于登录加载状态的状态
   const [loginLoading, setLoginLoading] = useState(false);
 
+  // Define a function for user login
+  // 定义用于用户登录的函数
   const login = useCallback(async (payload: API.LoginParams): Promise<API.LoginResult | null> => {
+    // Set the login loading state to true
+    // 将登录加载状态设置为 true
     setLoginLoading(true);
+
     try {
+      // Call the signIn function to perform the login
+      // 调用 signIn 函数执行登录操作
       const response = await signIn(payload);
+
+      // Set the login loading state to false and return the login response data
+      // 将登录加载状态设置为 false 并返回登录响应数据
       setLoginLoading(false);
       return response.data || null;
     } catch (error) {
+      // Log any errors and set the login loading state to false
+      // 记录任何错误并将登录加载状态设置为 false
       console.log(error);
       setLoginLoading(false);
       return null;
     }
   }, []);
 
+  // Define a function for user logout
+  // 定义用于用户注销的函数
   const logout = useCallback(() => {
+    // Call the signOut function to perform the logout
+    // 调用 signOut 函数执行注销操作
     signOut();
   }, []);
 
+  // Return the login loading state, login function, and logout function
+  // 返回登录加载状态、登录函数和注销函数
   return {
     loginLoading,
     login,
--- src/pages/UserLogin/index.tsx
+++ src/pages/UserLogin/index.tsx
@@ -6,15 +6,29 @@ import React from 'react';
 import { flushSync } from 'react-dom';
 import { history, useModel } from 'umi';
 
+// Define a Login component as a functional component
 const Login: React.FC = () => {
+  // Access the login and initialState functions from useModel hook
   const { login } = useModel('userLoginIndex');
   const { initialState, setInitialState } = useModel('@@initialState');
 
+  // Define handleLogin function which will be called when the form is submitted
+  // 定义 handleLogin 函数，用于表单提交时调用
   const handleLogin = async (values: any) => {
+    // Call the login function and store the response in the response variable
+    // 调用 login 函数，并将响应存储在 response 变量中
     const response = await login(values);
+    // If the response is null or undefined, return from the function
+    // 如果响应为 null 或 undefined，则从函数中返回
     if (!response) return;
+    // Call the fetchUserInfo function from the initialState object and store the response in the userResponse variable
+    // 从 initialState 对象中调用 fetchUserInfo 函数，并将响应存储在 userResponse 变量中
     const userResponse = await initialState?.fetchUserInfo?.();
+    // If the userResponse is null or undefined, return from the function
+    // 如果 userResponse 为 null 或 undefined，则从函数中返回
     if (!userResponse) return;
+    // Update the state with the new user data and redirect to the home page
+    // 使用新的用户数据更新状态并重定向到主页
     flushSync(() => {
       setInitialState((s) => ({
         ...s,
@@ -24,13 +38,21 @@ const Login: React.FC = () => {
     });
   };
 
+  // Render the login form wrapped in a PageContainer component
+  // 在 PageContainer 组件中渲染登录表单
   return (
     <PageContainer>
+      {/* Style the form container */}
+      {/* 样式表单容器 */}
       <div style={{ width: '50%', margin: '0 auto' }}>
+        {/* Define the login form using the ProForm component */}
+        {/* 使用 ProForm 组件定义登录表单 */}
         <ProForm
           onFinish={handleLogin}
           submitter={{
             render: (_, dom) => (
+              // Add a link to forget password and render the submit button
+              // 添加忘记密码链接并渲染提交按钮
               <>
                 <Button type="link" style={{ float: 'right' }}>
                   忘记密码
@@ -44,6 +66,8 @@ const Login: React.FC = () => {
             },
           }}
         >
+          {/* Define two tabs for account and mobile login */}
+          {/* 定义帐户和手机号登录的两个选项卡 */}
           <Tabs
             defaultActiveKey="accountLogin"
             items={[
@@ -52,6 +76,8 @@ const Login: React.FC = () => {
                 key: 'accountLogin',
                 children: (
                   <>
+                    {/* Define the username input field */}
+                    {/* 定义用户名输入 */}
                     <ProFormText
                       name="username"
                       placeholder="用户名"
@@ -65,6 +91,8 @@ const Login: React.FC = () => {
                         },
                       ]}
                     />
+                    {/* Define the password input field */}
+                    {/* 定义密码输入字段 */}
                     <ProFormText.Password
                       name="password"
                       placeholder="密码"
@@ -86,6 +114,8 @@ const Login: React.FC = () => {
                 key: 'mobileLogin',
                 children: (
                   <>
+                    {/* Define the mobile number input field */}
+                    {/* 定义手机号输入字段 */}
                     <ProFormText
                       name="mobile"
                       placeholder="手机号"
@@ -99,6 +129,8 @@ const Login: React.FC = () => {
                         },
                       ]}
                     />
+                    {/* Define the captcha input field */}
+                    {/* 定义验证码输入字段 */}
                     <ProFormText
                       name="captcha"
                       placeholder="验证码"
@@ -123,4 +155,6 @@ const Login: React.FC = () => {
   );
 };
 
+// Export the Login component as default
+// 导出默认的 Login 组件
 export default Login;
